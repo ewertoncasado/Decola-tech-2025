aa2ecb616fa76b4c177fd5f59e7e0c7a56080898
ewertoncasado <tom_casado@hotmail.com>
2025-03-08 02:06:06 -0300

    Initial commit

---
+++ src/main/java/DT2025/Decola/tech/_5/DecolaTech2025Application.java
@@ -0,0 +1,13 @@
+package DT2025.Decola.tech._5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecolaTech2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecolaTech2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/DT2025/Decola/tech/_5/DecolaTech2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package DT2025.Decola.tech._5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecolaTech2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
